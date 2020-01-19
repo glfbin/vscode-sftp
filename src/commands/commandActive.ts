f66819d4822925bc5ecd2f8f4f6e67e80751b9c2
glfbin <glfbin@163.com>
2020-01-19 16:44:05 +0800

    Update commandActive.ts
    active commad

--- src/commands/commandActive.ts
+++ src/commands/commandActive.ts
@@ -5,10 +5,5 @@ import { checkCommand } from './abstract/createCommand';
 export default checkCommand({
   id: 'sftp.active',
 
-  async handleCommand() {
-    vscode.commands.executeCommand(
-      'sftp.download',
-      ['/wlan_wlan_v2r19c10_master/ccjson_5paw5bu65paH5pys5paH5qGj.txt']
-    );
-  },
+  async handleCommand() {},
 });
